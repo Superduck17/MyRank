0e1a6d7d39b186da7502dd78b3b496ab91090676
Superduck17 <cooper@nervick.com>
2024-06-04 19:40:03 -0500

    Worked on Zach Bryan Page

--- JS1.js
+++ JS1.js
@@ -1,3 +1,15 @@
 document.getElementById("logo").addEventListener("click", function() {
-    window.location.href = "index.html";
+    window.location.href = "MyRank.html";
 });
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      document.getElementById("logo").style.scale = 0.5;
+      document.getElementById("home").style.display = 'none';
+    } else {
+      document.getElementById("logo").style.scale = 1;
+      document.getElementById("home").style.display = "flex";
+    }
+  }
